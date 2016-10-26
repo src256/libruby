65f920a1903791a9a973b8a1bdd98edca6f752f5
src <src@srcw.net>
2016-10-26 16:36:29 +0900

    changes

--- lib/libruby/misc_utils.rb
+++ lib/libruby/misc_utils.rb
@@ -1,4 +1,14 @@
+# coding: utf-8
 module Libruby
-
-
+  class MiscUtils
+    def self.rescue_mysql2_error(e)
+      if e.message =~ /Incorrect string value:/
+        # utf8mb4にすればいいのだ…。とりあえずそのばしのぎ。
+        puts e.message[0, 100]
+      else
+        puts e.message
+        puts e.backtrace
+      end
+    end
+  end
 end
