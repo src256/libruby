18fa166b740f91690dca0ae7fd581e4b5664a62f
src <src@srcw.net>
2018-04-16 14:44:28 +0900

    remove unit converter

--- lib/libruby.rb
+++ lib/libruby.rb
@@ -2,7 +2,7 @@ require "libruby/version"
 require "libruby/file_utils"
 require "libruby/misc_utils"
 require "libruby/multi_io"
-require "libruby/unit_converter"
+
 
 module Libruby
   # Your code goes here...
